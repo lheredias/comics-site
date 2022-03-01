f7f811c628bfcab4bdde639c9ce2d8bbf931572a
lheredias <herediasotelo@gmail.com>
2022-02-28 20:49:33 -0500

    none

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://pacific-reaches-19144.herokuapp.com"]
 
 
 # Application definition
