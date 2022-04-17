0e19b36bbc017c83a1de2e08beb8c89bc4f9524f
lheredias <herediasotelo@gmail.com>
2022-04-17 05:01:58 -0500

    FIX: anything

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -32,10 +32,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRETKEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
